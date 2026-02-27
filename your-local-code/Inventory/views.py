b781a65820c3b1def23c9ac4ab587ddabebac3e3
armydillo2025 <58220923+armydillo2025@users.noreply.github.com>
2026-02-27 11:51:17 -0700

    Add home and about view functions in Inventory

--- your-local-code/Inventory/views.py
+++ your-local-code/Inventory/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Create your views here.def home(request):
+    """Renders the Home page."""
+    return render(request, 'home.html')
+
+def about(request):
+    """Renders the About page."""
+    return render(request, 'about.html')
