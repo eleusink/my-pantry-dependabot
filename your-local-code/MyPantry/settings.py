6a4a1fc3d1f413fd70e1d0a58eecf6b22f3790e6
armydillo2025 <58220923+armydillo2025@users.noreply.github.com>
2026-03-14 11:26:47 -0600

    Update variable for Project Secret Key
    Fixes :
    `django.core.exceptions.ImproperlyConfigured: The SECRET_KEY must not be empty.`
    
    Mismatched Key variables `SECRET_KEY` vs `DJANGO_SECRET_KEY`
    
    Closes 128

--- your-local-code/MyPantry/settings.py
+++ your-local-code/MyPantry/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / ".env")
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 if not SECRET_KEY:
-    raise ImproperlyConfigured("The SECRET_KEY must not be empty.")
+    raise ImproperlyConfigured("The DJANGO_SECRET_KEY must not be empty.")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Check whether we're in DEBUG mode (Render environment variable)
